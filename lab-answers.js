a840ab07cbd002a7c82d2686b89f4da52f93cce5
Travis James <itravisjames@outlook.com>
2023-06-01 13:01:52 -0700

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -2,17 +2,41 @@
 // Easy Going
 ////////////////////////////////
 
-for ( let num = 1; num <= 20; num++)
+for (let num = 1; num <= 20; num++)
    
    
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
 
-for (let num = 2; num <= 200; num+=2) 
+for (let num = 2; num <= 200; num+=2)
 
 
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
 
+
+for (let num = 1; num <= 100; num++) {
+
+    if (num % 3 === 0 && num % 5 === 0) {
+
+        console.log("Fizz Buzz");
+    }
+
+    else if(num % 3 === 0) {
+
+        console.log("Fizz");
+    }
+
+    else if(num % 5 === 0) {
+
+ 
+        console.log("Buzz");
+
+   }
    
+}
+
 
 
