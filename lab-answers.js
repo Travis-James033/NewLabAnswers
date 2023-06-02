0849172f95c297afc07aa93b3329fdb60db36cbf
Travis James <itravisjames@outlook.com>
2023-06-01 22:21:49 -0700

    Wild Wild Life corrected

--- lab-answers.js
+++ lab-answers.js
@@ -1,8 +1,10 @@
 ////////////////////////////////
 // Easy Going
 ////////////////////////////////
+/*
+ for (let num = 1; num <= 20; num++)
 
-for (let num = 1; num <= 20; num++)
+ // console.log(num);
    
    
 ////////////////////////////////
@@ -10,6 +12,8 @@ for (let num = 1; num <= 20; num++)
 ////////////////////////////////
 
 for (let num = 2; num <= 200; num+=2)
+
+//console.log(num);
 
 
 ////////////////////////////////
@@ -42,42 +46,45 @@ for (let num = 2; num <= 200; num+=2)
 // Wild Wild life
 ////////////////////////////////
 
+*/
 
 const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
 const sharky = ["Sharky", "shark", 20, "Left Coast"]
 const plantee = ["Plantee", "plant",  5000 , "Mordor"]
 const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"] 
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]  
 
 
 for ( let i = 0; i < wolfy.length; i++ );
 
+//Plantee just had her birthday; change Plantee's array to reflect her being a year older.
 
 if ( i = 2 ) {
 
-    wolfy.shift('wolfy');
-    wolfy.unshift('Gameboy')
+    wolfy[i] = 17;
 } 
 
-
+//Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy"
 
 if ( i = 0 ) {
 
     wolfy[0].splice=(0, 1, "Gameboy");
 } 
 
+//Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+
 if ( i = 3 ) {
 
     wolfy[i] = "Gotham City"
 }
 
+//Give D'Art a second hometown by adding "Hawkins"
 for ( let i = 0; i < dart.length; i++ ); 
 
 if ( i = 3) {
 
     dart.push( "Hawkins ")
 }
- 
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
@@ -93,5 +100,3 @@ console.log(i.toUpperCase())
 }
 
 
-
-    
--- lab-test.js
+++
@@ -1,5 +0,0 @@
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-
-for ( let i = 0; i < wolfy.length; i++ );
-
-console.log(wolfy);
